19f26614f2f38296645bdf03ecbf5a2f9e157731
Callum <Brogancsp@gmail.com>
2025-12-29 04:14:05 +0100

    Player State

---
+++ Source/ArenaGame/Private/Player/AG_PlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Callum Brogan
+
+
+#include "ArenaGame/Public/Player/AG_PlayerState.h"
+
+AAG_PlayerState::AAG_PlayerState()
+{
+	SetNetUpdateFrequency(100.f);
+}
---
+++ Source/ArenaGame/Public/Player/AG_PlayerState.h
@@ -0,0 +1,19 @@
+// Copyright Callum Brogan
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AG_PlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENAGAME_API AAG_PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+public:
+	
+	AAG_PlayerState();
+};
